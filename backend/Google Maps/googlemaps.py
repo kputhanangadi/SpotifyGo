98d0fb1529a908225c08ffc0f7507811ff904909
Kevin Puthanangadi <kputhanangadi@gmail.com>
2023-01-28 00:29:37 -0800

    google maps js

--- backend/Google Maps/googlemaps.py
+++ backend/Google Maps/googlemaps.py
@@ -6,20 +6,19 @@ import os
 load_dotenv()
 
 # API KEY
-
 google_api_key = os.getenv("googlekey")
 
 # home address
-home_address = input("Enter a Home adress:\n")
+home_address = ['New York, NY'] # ("Enter a Home adress:\n")
 
 # work address
-work_address = input("Enter a Work adress:\n")
+work_address = ['Washington, DC'] # input("Enter a Work adress:\n")
 
 # base url
-url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&?"
+google_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 
 # get response
-r = requests.get(url + "origins=" + home_address + "&destinations=" + work_address + "&key=" + google_api_key)
+r = requests.get(google_url + "origins=" + home_address + "&destinations=" + work_address + "&key=" + google_api_key)
 
 #return time as text in seconds
 time = r.json()["rows"][0]["elements"][0]["duration"]["text"]
