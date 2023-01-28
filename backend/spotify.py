5ba52f0cbd23e8c7a4dfe8d8924a2393b5410cfe
Kevin Puthanangadi <kputhanangadi@gmail.com>
2023-01-27 17:54:23 -0800

    client id

--- backend/spotify.py
+++ backend/spotify.py
@@ -1,1 +1,4 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='ef99958a61dc48ae90da5e2d54eb6084', client_secret='eeef9b2ed97e405bbdd7bc57dd64d0dd', redirect_uri='your_redirect_uri', scope=['user-library-read', 'playlist-read-private']))
