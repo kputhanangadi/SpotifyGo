cbc6a09ea7a3de564fcb53e34a448740309f5855
ryanyap <ryandyap@icloud.com>
2023-01-29 00:00:00 -0800

    fadsfsd

--- frontend/src/routes/destination.js
+++ frontend/src/routes/destination.js
@@ -3,36 +3,25 @@ import { useState } from "react";
 import { Grid } from "@mui/material";
 import GenericButton from "../components/GenericButton";
 import { TextField } from "@mui/material";
-import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
 import { useEffect } from "react";
-import useGoogle from "react-google-autocomplete/lib/usePlacesAutocompleteService";
 
 export default function DestinationPage() {
-  const [addresses, setAddresses] = useState([]);
-  const {
-    placesService,
-    placePredictions,
-    getPlacePredictions,
-    isPlacePredictionsLoading,
-  } = usePlacesService({
-    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  });
 
-  useEffect(() => {
-    // fetch place details for the first element in placePredictions array
-    if (placePredictions.length)
-      placesService?.getDetails(
-        {
-          placeId: placePredictions[0].place_id,
-        }
-        // (placeDetails) => savePlaceDetailsToState(placeDetails)
-      );
-  }, [placePredictions]);
+  // useEffect(() => {
+  //   // fetch place details for the first element in placePredictions array
+  //   if (placePredictions.length)
+  //     placesService?.getDetails(
+  //       {
+  //         placeId: placePredictions[0].place_id,
+  //       }
+  //       // (placeDetails) => savePlaceDetailsToState(placeDetails)
+  //     );
+  // }, [placePredictions]);
 
   return (
     <div className="grid-container center-font">
       <div className="grid-item">
-        <TextField id="outlined-basic" label="Start" variant="outlined" />
+        <TextField id="filled-basic" label="Start" variant="filled" />
         {/* <Input.Search
           style={{ color: "black" }}
           value={value}
@@ -45,15 +34,13 @@ export default function DestinationPage() {
         /> */}
       </div>
       <div className="grid-item">
-        <TextField id="outlined-basic" label="Destination" variant="outlined" />
+        <TextField id="filled-basic" label="Destination" variant="filled" />
       </div>
-      <div className="grid-item">
         <GenericButton
           className="grid-item"
           href={"http://localhost:3000/selection"}
           text={"New Playlist"}
         />
       </div>
-    </div>
   );
 }
