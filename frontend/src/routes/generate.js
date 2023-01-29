6b2aa364decdb903b403d4542b00565c0d687f35
ryanyap <ryandyap@icloud.com>
2023-01-29 01:44:48 -0800

    fdasfdsf

--- frontend/src/routes/generate.js
+++ frontend/src/routes/generate.js
@@ -39,6 +39,9 @@ export default function GeneratePage() {
         </b>
       </p>
       </div>
+      <div className="header">
+        <img className="gen-img" src="https://www.shorturl.at/img/shorturl-icon.png"></img>
+      </div>
     </>
   );
 }
