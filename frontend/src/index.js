776164da28e27657aef76dc784c97e363ee88203
Kevin Puthanangadi <kputhanangadi@gmail.com>
2023-01-28 19:58:36 -0800

    nothing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
-import Root from "./routes/root";
 import Home from "./routes/home";
 import SelectionGrid from "./routes/selection";
 import ErrorPage from "./routes/error";
-import Contact from "./routes/contact";
 import DestinationPage from "./routes/destination";
 
 const router = createBrowserRouter([
