5d48950237f61d151ea764eed5de178e74c8a738
im-calvin <kelvinwong0519@gmail.com>
2023-01-28 23:30:16 -0800

    autofill destination.js

--- frontend/src/routes/destination.js
+++ frontend/src/routes/destination.js
@@ -6,52 +6,34 @@ import { TextField } from "@mui/material";
 import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
 import { useEffect } from "react";
 import useGoogle from "react-google-autocomplete/lib/usePlacesAutocompleteService";
+import SearchBar from "../components/AutoComplete";
 
 export default function DestinationPage() {
-  const [addresses, setAddresses] = useState([]);
-  const {
-    placesService,
-    placePredictions,
-    getPlacePredictions,
-    isPlacePredictionsLoading,
-  } = usePlacesService({
-    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  });
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
 
-  useEffect(() => {
-    // fetch place details for the first element in placePredictions array
-    if (placePredictions.length)
-      placesService?.getDetails(
-        {
-          placeId: placePredictions[0].place_id,
-        }
-        // (placeDetails) => savePlaceDetailsToState(placeDetails)
-      );
-  }, [placePredictions]);
+  const handleGetOrigin = (place) => {
+    setOrigin(place);
+  };
+
+  const handleGetDestination = (place) => {
+    setDestination(place);
+  };
 
   return (
     <div className="grid-container center-font">
       <div className="grid-item">
-        <TextField id="outlined-basic" label="Start" variant="outlined" />
-        {/* <Input.Search
-          style={{ color: "black" }}
-          value={value}
-          placeholder="Debounce 500 ms"
-          onChange={(evt) => {
-            getPlacePredictions({ input: evt.target.value });
-            setValue(evt.target.value);
-          }}
-          loading={isPlacePredictionsLoading}
-        /> */}
+        <SearchBar getPlaceFunc={handleGetOrigin} location={"Start"} />
       </div>
       <div className="grid-item">
-        <TextField id="outlined-basic" label="Destination" variant="outlined" />
+        <SearchBar getPlaceFunc={handleGetDestination} location={"End"} />
       </div>
       <div className="grid-item">
         <GenericButton
           className="grid-item"
-          href={"http://localhost:3000/selection"}
+          // href={"http://localhost:3000/selection"}
           text={"New Playlist"}
+          // onChange={}
         />
       </div>
     </div>
